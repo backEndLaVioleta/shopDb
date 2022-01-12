b4288c49ecf938553f9aec06e438d24bea7fbbd0
naranjito72 <theclick36@gmail.com>
2022-01-12 11:50:45 +0100

    swagger module added and working

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // cors
+  app.enableCors();
+  // prefix
+  const globalPrefix = 'api/v1';
+  app.setGlobalPrefix(globalPrefix);
+
+  // config swagger
+  const APP_NAME = process.env.NAME;
+  const VERSION = process.env.VERSION;
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle(APP_NAME)
+    .setDescription('Back-End for an Angular shop project')
+    .setVersion(VERSION)
+    .addTag('shopDb')
+    .build();
+
+  const docSwagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('test', app, docSwagger);
+  try {
+    const port = process.env.PORT || 3000;
+    await app.listen(port, () => {
+      logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    });
+  } catch (error) {
+    throw new Error(`${error.message}`);
+  }
 }
 bootstrap();
